7c8f1df6601ace05c21230f5952b4220e1fa28b3
Bjoern Schilberg <bjoern.schilberg@intevation.de>
2020-07-17 17:06:24 +0200

    Initial xlsx version

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -13,6 +13,8 @@ import Popup from "./Popup";
 import germany from "./germany";
 import "leaflet.markercluster";
 import "leaflet.markercluster/dist/MarkerCluster.css";
+import XLSX from 'xlsx';
+import GeoJSON from 'geojson';
 
 //https://github.com/KoRiGaN/Vue2Leaflet/issues/28
 
@@ -28,7 +30,7 @@ export default {
   components: { Popup },
   props: {},
   data: () => ({
-    url: "data/EinsatzstellenKartenanwendung.geojson",
+    url: "https://mapserver.nabu.de/owncloud/index.php/s/WSgbJsvRYH7jf6Z/download",
     einsatzstellen: {},
     einsatzstelle: {},
     einsatzstellen_besetzt: {},
@@ -40,7 +42,7 @@ export default {
       "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
       {
         attribution:
-          '&copy;<a href="http://www.esri.com/">Esri</a>i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community, '+process.env.VUE_APP_GIT_HASH,
+          '&copy;<a href="http://www.esri.com/">Esri</a>i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community, ' + process.env.VUE_APP_GIT_HASH,
         maxZoom: 18
       }
     ),
@@ -48,13 +50,13 @@ export default {
       minZoom: 5,
       maxZoom: 18,
       attribution:
-        'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, '+process.env.VUE_APP_GIT_HASH
+        'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, ' + process.env.VUE_APP_GIT_HASH
     }),
     streetmap: L.tileLayer(
       "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
       {
         attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>, '+process.env.VUE_APP_GIT_HASH,
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>, ' + process.env.VUE_APP_GIT_HASH,
         minZoom: 5,
         maxZoom: 18,
         id: "mapbox.streets",
@@ -66,7 +68,7 @@ export default {
     satellite: L.tileLayer.wms("https://tiles.maps.eox.at/?", {
       layers: "s2cloudless_3857",
       attribution:
-        '<a href="https://s2maps.eu" target="_blank">Sentinel-2 cloudless - https://s2maps.eu</a> by <a href="https://eox.at/" target="_blank">EOX IT Services GmbH</a> (Contains modified Copernicus Sentinel data 2017 & 2018), '+process.env.VUE_APP_GIT_HASH
+        '<a href="https://s2maps.eu" target="_blank">Sentinel-2 cloudless - https://s2maps.eu</a> by <a href="https://eox.at/" target="_blank">EOX IT Services GmbH</a> (Contains modified Copernicus Sentinel data 2017 & 2018), ' + process.env.VUE_APP_GIT_HASH
     })
     //#ba3b76
     //#b54076
@@ -97,7 +99,7 @@ export default {
   mounted() {
     this.map = L.map("map", {
       attributionControl: false,
-      center: (L.Browser.mobile ? [51,10]:[51, 13]),
+      center: (L.Browser.mobile ? [51, 10] : [51, 13]),
       zoom: (L.Browser.mobile ? 5 : 6),
       maxZoom: 18,
       minZoom: (L.Browser.mobile ? 5 : 6),
@@ -130,7 +132,7 @@ export default {
         outerBounds: new L.LatLngBounds([-90, -360], [90, 360])
       },
 
-      initialize: function(latLngs, options) {
+      initialize: function (latLngs, options) {
         var outerBoundsLatLngs = [
           this.options.outerBounds.getSouthWest(),
           this.options.outerBounds.getNorthWest(),
@@ -145,7 +147,7 @@ export default {
       }
     });
 
-    L.mask = function(latLngs, options) {
+    L.mask = function (latLngs, options) {
       return new L.Mask(latLngs, options);
     };
 
@@ -154,10 +156,41 @@ export default {
   methods: {
     fetchData(url) {
       fetch(url)
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
+        .then(function (response) {
+          /* get the data as a Blob */
+          if (!response.ok) throw new Error("fetch failed");
+          return response.arrayBuffer();
+        }).then(function (ab) {
+          /* parse the data when it is received */
+          //var data = new Uint8Array(ab);
+          var workbook = XLSX.read(new Uint8Array(ab), { type: "array" });
+          var ws = workbook.Sheets[workbook.SheetNames[0]];
+          ws.A1.w = "titel";
+          ws.B1.w = "teaser";
+          ws.C1.w = "status";
+          ws.D1.w = "name";
+          ws.E1.w = "adresse";
+          ws.F1.w = "themen";
+          ws.G1.w = "beschreibung";
+          ws.H1.w = "taschengeld";
+          ws.I1.w = "unterkunft";
+          ws.J1.w = "anforderungen";
+          ws.K1.w = "beginn";
+          ws.L1.w = "ende";
+          ws.M1.w = "bewerbungsschluss";
+          ws.N1.w = "anzahl";
+          ws.O1.w = "ansprechpartner"
+          ws.P1.w = "telefon";
+          ws.Q1.w = "email";
+          ws.R1.w = "webseite";
+          ws.S1.w = "bild"
+          ws.T1.w = "lat";
+          ws.U1.w = "lon";
+          var js = XLSX.utils.sheet_to_json(ws, { range: 0 , defval: ""});
+          return js;
+        }).then(data => {
+          return GeoJSON.parse(data, { Point: ['lat', 'lon'] });
+        }).then(gj => {
           var greenIcon = new L.Icon({
             iconUrl: "img/marker-icon-green.png",
             iconSize: [25, 41],
@@ -176,44 +209,44 @@ export default {
             iconAnchor: [12, 41],
             popupAnchor: [1, -34],
           });
-          this.einsatzstellen = L.geoJSON(data, {
+          this.einsatzstellen = L.geoJSON(gj, {
             onEachFeature: this.onEachFeatureClosure(),
             //filter: function(feature, layer)
-            filter: function(feature) {
+            filter: function (feature) {
               return feature.properties.status == "frei";
             },
-            pointToLayer: function(feature, latlng) {
+            pointToLayer: function (feature, latlng) {
               return L.marker(latlng, {
                 icon: greenIcon
-              }).on("mouseover", function() {
+              }).on("mouseover", function () {
                 this.bindPopup(feature.properties.name).openPopup();
               });
             }
           });
-          this.einsatzstellen_besetzt = L.geoJSON(data, {
+          this.einsatzstellen_besetzt = L.geoJSON(gj, {
             onEachFeature: this.onEachFeatureClosure(),
             //filter: function(feature, layer)
-            filter: function(feature) {
+            filter: function (feature) {
               return feature.properties.status == "besetzt";
             },
-            pointToLayer: function(feature, latlng) {
+            pointToLayer: function (feature, latlng) {
               return L.marker(latlng, {
                 icon: redIcon
-              }).on("mouseover", function() {
+              }).on("mouseover", function () {
                 this.bindPopup(feature.properties.name).openPopup();
               });
             }
           });
-          this.einsatzstellen_unklar = L.geoJSON(data, {
+          this.einsatzstellen_unklar = L.geoJSON(gj, {
             onEachFeature: this.onEachFeatureClosure(),
             //filter: function(feature, layer)
-            filter: function(feature) {
+            filter: function (feature) {
               return (!(feature.properties.status == "frei") && !(feature.properties.status == "besetzt"));
             },
-            pointToLayer: function(feature, latlng) {
+            pointToLayer: function (feature, latlng) {
               return L.marker(latlng, {
                 icon: greyIcon
-              }).on("mouseover", function() {
+              }).on("mouseover", function () {
                 this.bindPopup(feature.properties.name).openPopup();
               });
             }
@@ -222,7 +255,7 @@ export default {
           var markers = L.markerClusterGroup({
             showCoverageOnHover: false,
             maxClusterRadius: 25,
-            iconCreateFunction: function(cluster) {
+            iconCreateFunction: function (cluster) {
               return L.divIcon({
                 html:
                   '<div class="divIconCluster"></div><div class="myMarkerCluster">' +
@@ -247,7 +280,7 @@ export default {
     onEachFeatureClosure() {
       let that = this;
       return function onEachFeature(feature, layer) {
-        layer.on("click", function(e) {
+        layer.on("click", function (e) {
           that.klick(e.target);
         });
       };
